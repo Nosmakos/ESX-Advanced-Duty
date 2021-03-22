51e94c5efb85711702e174f079e2021216fea506
joshua24h <37049738+joshua24h@users.noreply.github.com>
2021-03-22 02:02:24 +0100

    Update config.lua
    The previous police coordinates were below the ground

--- esx_advanced_duty/config.lua
+++ esx_advanced_duty/config.lua
@@ -7,7 +7,7 @@ Config.Zones = {
 
   Police = 
   {
-    Pos   = {x = 462.0, y = -995.84, z = 29.70},
+    Pos   = {x = 459.65, y = -991.08, z = 29.70},
     Size = { x = 1.5, y = 1.5, z = 1.5 },
     Color = { r = 191, g = 255, b = 0 },
     Type  = 27,
